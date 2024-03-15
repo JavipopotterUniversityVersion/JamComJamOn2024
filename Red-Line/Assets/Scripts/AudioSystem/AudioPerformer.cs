d73741e95d048bf05cbb387db32b1af5dfc9c3d1
Javipopotter <javivals@ucm.es>
2024-03-15 17:30:00 +0100

    added audio system

---
+++ Red-Line/Assets/Scripts/AudioSystem/AudioPerformer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPerformer : MonoBehaviour
+{
+
+    AudioPlayer[] _audioPlayer;
+    static private AudioPerformer _instance;
+    private void Awake()
+    {
+        Screen.SetResolution(1280, 960, true);
+
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            _audioPlayer = Resources.LoadAll<AudioPlayer>("SoundPlayers");
+
+            for (int i = 0; i < _audioPlayer.Length; i++)
+            {
+                AudioSource currentSource = gameObject.AddComponent<AudioSource>();
+
+                _audioPlayer[i].OnAudioPlay.
+                AddListener((AudioClip clip, float volume, float pitch) =>
+                    {
+                        currentSource.volume = volume;
+                        currentSource.pitch = pitch;
+                        currentSource.clip = clip;
+                        currentSource.Play();
+                    }
+                );
+
+                _audioPlayer[i].OnAudioStop.AddListener(() => currentSource.Stop());
+            }
+        }
+    }
+}
