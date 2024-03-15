69d178ac91513571e46ff31a91e1e276b0b6b807
Javipopotter <javivals@ucm.es>
2024-03-15 17:21:44 +0100

    fixed input bug

--- Red-Line/Assets/PrototypeScripts/WebReleaser.cs
+++ Red-Line/Assets/PrototypeScripts/WebReleaser.cs
@@ -7,6 +7,7 @@ public class WebReleaser : MonoBehaviour
     HingeJoint2D hingeJoint2D;
     [SerializeField] float releaseFactor = 1;
     SpriteRenderer sr;
+    float value;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +15,23 @@ public class WebReleaser : MonoBehaviour
         sr = GetComponentInChildren<SpriteRenderer>();
     }
 
+    private void Update() {
+        float anchorValue = hingeJoint2D.connectedAnchor.x + value;
+
+        if(anchorValue >= 0) hingeJoint2D.connectedAnchor = new Vector2(anchorValue, 0);
+
+        sr.transform.localScale = new Vector3(anchorValue * 2, anchorValue * 2, 1);
+    }
+
     public void ThrowWeb(float sum)
     {
+        if(sum != 0) sum = Mathf.Sign(sum);
+
         Vector2 connectedDirection = (Vector2)hingeJoint2D.connectedBody.transform.position - (Vector2)transform.position;
         connectedDirection.Normalize();
 
-        float value = sum * releaseFactor * Time.deltaTime;
+        value = sum * releaseFactor * Time.deltaTime;
 
         if(connectedDirection.y < 0) value *= -1;
-
-        float anchorValue = hingeJoint2D.connectedAnchor.x + value;
-
-        hingeJoint2D.connectedAnchor = new Vector2(anchorValue, 0);
-        sr.transform.localScale = new Vector3(anchorValue * 2, anchorValue * 2, 1);
     }
 }
