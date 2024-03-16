f252c961ec77cd897140b81c02cd253b8be76b66
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2024-03-16 18:29:20 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Red-Line/Assets/Prefabs/Enemies/MorteroEnemigo/ParableBulletComponent.cs
+++ Red-Line/Assets/Prefabs/Enemies/MorteroEnemigo/ParableBulletComponent.cs
@@ -5,5 +5,47 @@ using UnityEngine;
 
 public class ParableBulletComponent : MonoBehaviour
 {
-    
+    private Transform _myTransform;
+
+    [SerializeField]
+    private float _downForce = 2;
+
+    [SerializeField]
+    private Vector3 _direction;
+
+    [SerializeField]
+    private int _damage = 0;
+
+    private void Update()
+    {
+        _direction = new Vector3(_direction.x, _direction.y - _downForce*Time.deltaTime, _direction.z);
+
+        _myTransform.position = _myTransform.position + _direction * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MovementStopper>() != null && collision.TryGetComponent<HealthComponent>(out HealthComponent a))
+        {
+            a.Damage(_damage);
+        }
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        _direction = direction;
+    }
+
+    private void Awake()
+    {
+        _myTransform = transform;
+        StartCoroutine(DestroyBullet());
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(3);
+
+        Destroy(gameObject);
+    }
 }
