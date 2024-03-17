42d146f7a75cb932a8121ec2198708b73c338cc9
Javipopotter <javivals@ucm.es>
2024-03-17 19:20:40 +0100

    Me estoy meando

--- Red-Line/Assets/Scripts/PowerUpSystem/ReverseInputPowerUp.cs
+++ Red-Line/Assets/Scripts/PowerUpSystem/ReverseInputPowerUp.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ReverseInputPowerUp : MonoBehaviour, IPowerUp
 {
     [SerializeField] private float _secondsToUnRevert;
+    [SerializeField] UnityEvent _onApply = new UnityEvent();
     private InputManager _playerInput;
     public void Apply()
     {
         _playerInput.ReverseControls(_secondsToUnRevert);
+        _onApply.Invoke();
         Destroy(this.gameObject);
     }
 
